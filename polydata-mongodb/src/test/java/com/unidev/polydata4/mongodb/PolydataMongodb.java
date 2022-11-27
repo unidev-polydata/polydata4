7bc8486ab7aad7e0a6a32b4078026f4efcc7c840
denis256 <denis.o@linux.com>
2022-11-27 12:58:00 +0200

    Basic mongodb test

---
+++ polydata-mongodb/src/test/java/com/unidev/polydata4/mongodb/PolydataMongodb.java
@@ -0,0 +1,14 @@
+package com.unidev.polydata4.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public class PolydataMongodb {
+
+    @Test
+    void polyCreation() {
+
+    }
+
+}
