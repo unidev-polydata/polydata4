cddeaae6bcc463d3d17864dd54617cd09baec364
denis256 <denis.o@linux.com>
2022-12-25 20:07:41 +0200

    yaml files parsing

--- polydata-flat-files/src/main/java/com/unidev/polydata4/flatfiles/yaml/YamlRepository.java
+++ polydata-flat-files/src/main/java/com/unidev/polydata4/flatfiles/yaml/YamlRepository.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
-public class PolydataYamlModel {
+public class YamlRepository {
     @Getter
     @Setter
     private String poly;
