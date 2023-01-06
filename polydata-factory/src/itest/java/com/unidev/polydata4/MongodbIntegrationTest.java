ad03f697a95e68dfefade1f424e10231a29bfc23
denis256 <denis.o@linux.com>
2023-01-06 20:41:49 +0200

    Common mongodb tests

--- polydata-factory/src/itest/java/com/unidev/polydata4/MongodbIntegrationTest.java
+++ polydata-factory/src/itest/java/com/unidev/polydata4/MongodbIntegrationTest.java
@@ -1,7 +1,31 @@
 package com.unidev.polydata4;
 
+import com.unidev.polydata4.domain.BasicPoly;
+import org.junit.jupiter.api.BeforeEach;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 public class MongodbIntegrationTest extends IntegrationTest {
+
+    @Container
+    private final GenericContainer mongodb = new GenericContainer("mongo:6.0.2-focal")
+            .withExposedPorts(27017);
+
+    @Container
+    private final GenericContainer redis = new GenericContainer("redis:7.0.5")
+            .withExposedPorts(6379);
+
+
+    @BeforeEach
+    public void setup() {
+        BasicPoly config = BasicPoly.newPoly()
+                .with("type", "mongodb")
+                .with("uri", "mongodb://localhost:" + mongodb.getMappedPort(27017) + "/polydata4")
+                ;
+        create(config);
+    }
+
+
 }
